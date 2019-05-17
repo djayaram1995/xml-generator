53ed6c79b01d8be5511d15ae5756f582b409a2a3
djayaram1995 <jayaramdeenu@gmail.com>
2019-05-17 07:28:35 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state={
+      url: ""
+    }
+  }
+  urlChange(state, e) {
+    this.setState({
+      [state]: e.target.value
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <label htmlFor="url">URL: </label>
+        <input name="url "type="text" value={this.state.url} onChange={e => this.urlChange("url", e)} />
+        <label htmlFor="parent">parent: </label>
+        <input name="parent "type="text" value={this.state.parent} onChange={e => this.urlChange("parent",e)} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
