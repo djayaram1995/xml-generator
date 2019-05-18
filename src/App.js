5eb356748e9fcccd849c95402aee76b6e61cf63b
djayaram1995 <jayaramdeenu@gmail.com>
2019-05-18 06:03:52 +0530

    done little

--- src/App.js
+++ src/App.js
@@ -1,29 +1,92 @@
-import React, {Component} from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 
 class App extends Component {
   constructor() {
     super();
-    this.state={
-      url: ""
-    }
+    this.state = {
+      url: "",
+      parent: "",
+      childs: []
+    };
   }
-  urlChange(state, e) {
+  urlChange = e => {
+    this.facilitator("url", e);
+  };
+  parentTag = e => {
+    this.facilitator("parent", e);
+  };
+  facilitator = (state, e) => {
     this.setState({
-      [state]: e.target.value
+        [state]: e.target.value
+      })
+    
+  };
+  addChild = () => {
+    this.setState({
+      childs: [...this.state.childs, {}]
+    });
+  };
+  addChildFinal = (e, i) => {
+    let childs = this.state.childs;
+    childs[i].name = e.target.value;
+    this.setState({ childs });
+  };
+  generateXml = () => {
+    let childs = "";
+    this.state.childs.map(child => {
+      childs = childs+`<${child.name}></${child.name}>`
+      return null;
     })
-  }
+    let xml = `<${this.state.parent}>
+    ${childs}
+    </${this.state.parent}>`
+    console.log(xml);
+  };
   render() {
     return (
       <div className="App">
         <label htmlFor="url">URL: </label>
-        <input name="url "type="text" value={this.state.url} onChange={e => this.urlChange("url", e)} />
-        <label htmlFor="parent">parent: </label>
-        <input name="parent "type="text" value={this.state.parent} onChange={e => this.urlChange("parent",e)} />
+        <input
+          name="url "
+          type="text"
+          value={this.state.url}
+          onChange={this.urlChange}
+        />
+        <div>
+          <label htmlFor="parent">parent: </label>
+          <input
+            name="parent "
+            type="text"
+            value={this.state.parent}
+            onChange={this.parentTag}
+          />
+          <button name="addChild" onClick={this.addChild}>
+            ADD CHILD
+          </button>
+        </div>
+        <div>
+          {this.state.childs.map((child, index) => {
+            return (
+              <div key={index}>
+                <label htmlFor={`child${index + 1}`}>{`child${index +
+                  1} :`}</label>
+                <input
+                  type="text"
+                  onChange={e => {
+                    this.addChildFinal(e, index);
+                  }}
+                />
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          <button onClick={this.generateXml}>GERNERATE XML</button>
+        </div>
       </div>
     );
   }
-  
 }
 
 export default App;
